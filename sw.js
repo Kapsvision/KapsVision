9aa6b3f587df41807003329b32cd49b6d0594cb3
Kapsvision <info@kapsvision.com>
2026-03-21 01:34:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
-const CACHE="kaps-v1";
+const CACHE_NAME="kaps-v2";
 
-self.addEventListener("install",e=>{
- e.waitUntil(
-  caches.open(CACHE).then(c=>c.addAll([
-   "index.html",
-   "logo.png",
-   "india-locations.js"
-  ]))
- );
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./logo.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch",e=>{
- e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request))
- );
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request);
+    })
+  );
 });
